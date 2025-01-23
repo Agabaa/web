46b1796247a4b87c24440b0750ed61d9ea97db8b
Agabaa <133192595+Agabaa@users.noreply.github.com>
2025-01-23 11:05:20 +0300

    switch statement

--- basics/.js
+++ basics/.js
@@ -110,3 +110,41 @@ for(let i=1;i<100;i++){
 for(i in fruits){
     console.log(fruits[i])
 }
+for(i of fruits){
+    console.log(i)
+}
+/**switch(param){
+ * case;
+ * break;
+ * 
+ * case;
+ * break;
+ * 
+ * default;
+}*/
+let day="monday"
+switch(day){
+    case "monday":
+        console.log("today is monday")
+        break;
+        case "tuesday":
+            console.log("today is tuesday")
+            break;
+            case "wednesday":
+                console.log("today is wednesday")
+                break;
+                case "thursday":
+                    console.log("today is thursday")
+                    break;
+                    case "friday":
+                        console.log("today is friday")
+                        break;
+                        case "saturday":
+                            console.log("today is saturday")
+                            break;
+                            case "sunday":
+                                console.log("today is sunday")
+                                break;
+                                default:
+                                    console.log("invalid day")
+                                    }
