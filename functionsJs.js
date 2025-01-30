2bdef1c4a37ea00844494a93f7bcbc7a2c87b8c5
Agabaa <133192595+Agabaa@users.noreply.github.com>
2025-01-30 20:31:21 +0300

    authentication

---
+++ functionsJs.js
@@ -0,0 +1,23 @@
+// returning functions 
+// function name(){
+
+// }
+
+// const name = (param) =>{
+//     return param;
+// }
+
+//void functions 
+/**function name(){
+    console.log()
+}
+const name =()=>{
+    console.log()
+
+}*/
+const user="agaba";
+function greet(user){
+    console.log(`hello ${user}`);
+
+}
+greet(user)
